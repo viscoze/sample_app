dc557f0746072f565c7565d6215c7da04c845f88
Viscoze <vlad.oganov@gmail.com>
2016-02-09 20:47:04 +0300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,16 +4,32 @@ describe "UserPages" do
 
   subject { page }
 
-  describe "signup page" do
+  describe "signup" do
 
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
+  describe "with invalid information" do
      it "should not create a user" do
        expect { click_button submit }.not_to change(User, :count)
      end
+
+     describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
 
     describe "with valid information" do
@@ -36,10 +52,6 @@ describe "UserPages" do
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
-  end
-
-  describe "" do
-
   end
 
 end
